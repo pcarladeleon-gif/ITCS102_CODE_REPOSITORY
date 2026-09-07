d3b6316d1248b27d4ce1c06a5347df209f28d943
pcarladeleon-gif <p.carladeleon@gmail.com>
2026-09-07 16:13:30 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,11 @@
+#concatenation = combining strings
+
+hobbies = "" #blank strings
+
+h = input("What are your hobbies --> ")
+hobbies += h + ", "
+
+h = input("What else --> ")
+hobbies += h + ", "
+
+print("my hobbies are as ", hobbies) 
